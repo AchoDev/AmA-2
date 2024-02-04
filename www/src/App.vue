b440f71d76bdf89fd8ab9b080cff7c63bae10f76
AchoDev <ahmedhdasi89@gmail.com>
2024-02-04 16:36:21 +0100

    initial commit

---
+++ www/src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+    <Dictionary 
+    
+      mainLang="spanish"
+      secondLang="german"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Dictionary from './DictionaryPage/Dictionary.vue'
+
+</script>
+
+<style>
+
+html {
+  min-height: 100vh;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
---
+++ www/src/DictionaryPage/Dictionary.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        
+        <nav>
+            <div>
+                <span>ama img</span>
+                <button> >> </button>
+            </div>
+
+            <div>{{mainLang}} - {{secondLang}}</div>
+
+            <div>
+                <button>...</button>
+            </div>
+        </nav>
+
+        <main>
+            <div v-for="letter in currentAlphabet">
+                {{letter}}
+            </div>
+
+        </main>
+
+        <div id="bottom-bar">
+            <button>search</button>
+
+            <div>
+                <button> &vartriangleleft; </button>
+
+                <span>Page {{ currentPage }}/{{ totalPages }}</span>
+
+                <button> &vartriangleright; </button>
+            </div>
+
+            <button>+ Add word</button>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';  
+
+import alphabets from './alphabets.ts'
+
+const props = defineProps<{
+    mainLang: string,
+    secondLang: string
+}>()
+
+const currentPage = ref(1)
+const totalPages = ref(1)
+
+const currentAlphabet = ref(alphabets[props.mainLang])
+
+</script>
+
+<style scoped lang="scss">
+
+$bar-height: 70px;
+
+nav {
+    height: $bar-height;
+    background: white;
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+main {
+    min-height: 100vh;
+    width: 100%;
+    background: linear-gradient(180deg, #ff8800, #ffcf76);
+}
+
+#bottom-bar {
+    background: white;
+    height: $bar-height;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+</style>
